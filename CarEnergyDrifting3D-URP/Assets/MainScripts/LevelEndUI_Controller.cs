733fa462212e4e6fb62ef2fc194dd847162f62f6
Kenan <kenan.binay@hotmail.com>
2022-03-30 03:50:28 +0300

    Home button and level text ui sprites added

---
+++ CarEnergyDrifting3D-URP/Assets/MainScripts/LevelEndUI_Controller.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEndUI_Controller : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
