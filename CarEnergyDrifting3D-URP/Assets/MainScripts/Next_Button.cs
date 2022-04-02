830bf8d75f17a8356c0605d7cb663c98bb563016
Kenan <kenan.binay@hotmail.com>
2022-04-02 18:40:38 +0300

    LevelEndUI sprites added

---
+++ CarEnergyDrifting3D-URP/Assets/MainScripts/Next_Button.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Next_Button : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+    public void NextLevel_Button()
+    {
+
+    }
+}
