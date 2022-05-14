5ce6588f220d99d93bb0da6aaeb8f005da18d3fb
Kenan <kenan.binay@hotmail.com>
2022-05-14 21:37:52 +0300

    next button script fixes and island model change

--- CarEnergyDrifting3D-URP/Assets/MainScripts/Next_Button.cs
+++ CarEnergyDrifting3D-URP/Assets/MainScripts/Next_Button.cs
@@ -8,58 +8,69 @@ public class Next_Button : MonoBehaviour
     [SerializeField] private Image _img;
     [SerializeField] private Sprite _default, _pressed;
     public static bool nextLevel_Button;
-    bool _buttonClick;
+    bool _buttonClick, pressControl;
     void Start()
     {
+        pressControl = false;
         _img.sprite = _default;
         _buttonClick = false;
     }
 
     public void NextLevel()
-    {    
-
+    {
         if (_buttonClick == false)
         {
-            _img.sprite = _pressed;
-            StartCoroutine(nextButtonDelay());
+            if (pressControl == false)
+            {
+                _img.sprite = _pressed;
+                StartCoroutine(nextButtonDelay());
+            }
+
         }
         else
         {
-            if (LevelController.currentLevel != 9) { LevelController.currentLevel++; }
+            if (pressControl == false)
+            {
+                if (LevelController.currentLevel != 9)
+                {
+                    LevelController.currentLevel++;
+                    pressControl = true;
+                }
+            }
 
             Debug.Log("next level  | currentLevel : " + LevelController.currentLevel);
 
-          /*  switch (LevelController.currentLevel)
-            {
-                case 0:
-                    SceneManager.LoadScene("levelMap_1");
-                    break;
-                case 1:
-                    SceneManager.LoadScene("levelMap_2");
-                    break;
-                case 2:
-                    SceneManager.LoadScene("levelMap_3");
-                    break;
-                case 3:
-                    SceneManager.LoadScene("levelMap_4");
-                    break;
-                case 4:
-                    SceneManager.LoadScene("levelMap_5");
-                    break;
-                case 5:
-                    SceneManager.LoadScene("levelMap_6");
-                    break;
-                case 6:
-                    SceneManager.LoadScene("levelMap_7");
-                    break;
-                case 7:
-                    SceneManager.LoadScene("levelMap_8");
-                    break;
-                case 8:
-                    SceneManager.LoadScene("levelMap_9");
-                    break;
-            }
-          */
+            /*  switch (LevelController.currentLevel)
+              {
+                  case 0:
+                      SceneManager.LoadScene("levelMap_1");
+                      break;
+                  case 1:
+                      SceneManager.LoadScene("levelMap_2");
+                      break;
+                  case 2:
+                      SceneManager.LoadScene("levelMap_3");
+                      break;
+                  case 3:
+                      SceneManager.LoadScene("levelMap_4");
+                      break;
+                  case 4:
+                      SceneManager.LoadScene("levelMap_5");
+                      break;
+                  case 5:
+                      SceneManager.LoadScene("levelMap_6");
+                      break;
+                  case 6:
+                      SceneManager.LoadScene("levelMap_7");
+                      break;
+                  case 7:
+                      SceneManager.LoadScene("levelMap_8");
+                      break;
+                  case 8:
+                      SceneManager.LoadScene("levelMap_9");
+                      break;
+              }
+            */
         }
     }
 
